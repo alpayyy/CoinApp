ee38c682b09dd3e733f031296cabe23b39953720
alpayyy <115874416+alpayyy@users.noreply.github.com>
2023-10-27 12:59:09 +0300

    update

---
+++ src/Components/CoinApp/Coin.jsx
@@ -0,0 +1,22 @@
+import './CoinStyles.css';
+import CoinTail from './tail.png';
+import CoinHead from './head.png';
+
+const Coin =({currentStatus,rotate})=>
+{
+    return(
+    <div className="Coin-container">
+     <div className={`Coin ${rotate ? 'Coin-rotate' : ''}`}>
+    <img
+    src={currentStatus==='yazi'? CoinTail:CoinHead}
+    alt=""
+    className={currentStatus==='yazi'?'Coin-back':'Coin-front'}
+    
+    
+    />
+     </div>
+    </div>
+    );
+};
+
+export default Coin;
